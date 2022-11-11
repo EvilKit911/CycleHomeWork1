715fba9829a3e67e709978e0b3a200f891b8cb77
Nikita <19nikita98.3939@gmail.com>
2022-11-11 20:28:27 +0200

    Цыклы 2 домашка задача 1, 2, 3, 4

--- src/Main.java
+++ src/Main.java
@@ -50,14 +50,61 @@ public class Main {
 
         System.out.println(" ");
 
-        //задание 8
-
+        //задание 8 и 9
+        // цикл 2 урок
+        //задание 1
         int salary = 29000;
         int total = 0;
-        for (int i = 1; i <= 12; i++) {
+        int i = 0;
+        while (total <= 2_459_000) {
+            i = i + 1 ;
             total = total + total/100; // сумма накоплений при накоплении с процентами
-            total = total + salary; // сумма накоплений
-            System.out.println("Месяц " + i + ", сумма накоплений равна " + total);
+            total = total + salary;// сумма накоплений
+
+            //System.out.println("Месяц " + i + ", сумма накоплений равна " + total);
+        }
+        System.out.println("Месяц " + i + ", сумма накоплений равна " + total);
+
+        System.out.println(" ");
+
+        //задание 2
+
+        int o = 0;
+        while (o < 10){
+            o = o + 1;
+            System.out.println(o);
+        }
+        System.out.println(" ");
+
+        for (;o > 0;o --){
+            System.out.println(o);
+        }
+
+        System.out.println(" ");
+
+        // задача 3
+        int y = 12_000_000;
+        int thousand = 0;// тысячи =)
+        int birth = 17;
+        int dead = 8;
+        for(i = 1;i <= 10;i++ ){
+            thousand = y / 12000; // привожу 12миллионов к тысячи
+            thousand = thousand + birth - dead;// произвожу нужные вычесления
+            y = thousand * 12000;// возвращаю полученное значение к милионам
+            System.out.println("Год " + i + " Численность населения составляет " + y);
+        }
+
+        System.out.println(" ");
+
+        // задача 4
+
+        int moneyVasa = 15000;// взнос под процент
+        int percent = 0;// процент
+        for(i = 1; moneyVasa < 12_000_000; i++){
+            percent = moneyVasa / 100 * 7;
+            moneyVasa = moneyVasa + percent;
+            System.out.println("Месяц " + i + " Накопления " + moneyVasa);
+
         }
 
 
